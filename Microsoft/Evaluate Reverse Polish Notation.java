a546a11917eaf309e486dd0163536a27c140c384
Morris Thakur <83053311+morris282001@users.noreply.github.com>
2023-01-03 23:28:21 +0530

    Update Evaluate Reverse Polish Notation.java

--- Microsoft/Evaluate Reverse Polish Notation.java
+++ Microsoft/Evaluate Reverse Polish Notation.java
@@ -1,1 +1,42 @@
+class Solution {
+    public int evalRPN(String[] tokens) {
+       
+        int t1=0;
+        int t2=0;
+        Stack<Integer> s = new Stack<Integer>();
+        for(int i=0; i<tokens.length; i++){
+            switch(tokens[i]){
 
+                case "+": t1 = s.pop();
+                t2 = s.pop();
+                
+                s.push(t1+t2);
+                break;
+
+                case "-": t1 = s.pop();
+                t2 = s.pop();
+                
+                s.push(t2-t1);
+                break;
+
+                case "*": t1 = s.pop();
+                t2 = s.pop();
+                
+                s.push(t1*t2);
+                break;
+
+                case "/": t1 = s.pop();
+                t2 = s.pop();
+                
+                s.push(t2/t1);
+                break;
+
+                default: s.push(Integer.parseInt(tokens[i]));
+
+            }
+        }
+
+    return s.peek();
+        
+    }
+}
